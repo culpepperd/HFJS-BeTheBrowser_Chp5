0a9f325be2a33ffc904d89529b4dd7d8a44bb2f6
culpepperd <culpepperd2490@gmail.com>
2023-06-06 12:27:54 -0400

    added song object and properties, and called play/pause methods

--- btb_chp5.js
+++ btb_chp5.js
@@ -1,6 +1,28 @@
 // create song object with name/artist/minutes/seconds/genre/playing properties
 // and play() and pause() methods
+var song = {
+	name: "Walk This Way",
+	artist:"Run-D.M.C.",
+	minutes: 4,
+	seconds: 3,
+	genre: "80's",
+	playing: false,
+	// play() method
+	play: function () {
+		if (!this.playing) {
+			this.playing = true;
+			console.log("Playing: " + this.name + " by, " + this.artist);
+		}
+	}, // need a comma after each property/method except the last
 
+	// pause() method
+	pause: function () {
+		if (this.playing) {
+			this.playing = false;
+		}
+	}
+};
 // call play() method
-
+song.play();
 // call pause() method
+song.pause();
